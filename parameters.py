e9905c5f5dd286f0a7ada96473d4a2f64b455820
Araime <alvonsa09121985@gmail.com>
2023-10-08 15:48:45 +0400

    Added fullscreen mode

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ from images import mushroom_img, mana_img, egg_img, meat_img, berries_img
 
 display_width = 800
 display_height = 600
-display = pygame.display.set_mode((display_width, display_height))
+display = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN)
 pygame.mouse.set_visible(False)
 
 clock = pygame.time.Clock()
